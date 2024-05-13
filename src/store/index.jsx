32c49f59fdab59b2bcfbd8bcd5c1bf35672e6b20
lucas-po <lucaschang1127@gmail.com>
2024-05-14 05:25:47 +0900

    feat: add store values

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,37 +1,40 @@
-import { createGlobalState } from 'react-hooks-global-state'
+import { createGlobalState } from "react-hooks-global-state";
 
 const { setGlobalState, useGlobalState, getGlobalState } = createGlobalState({
-  alert: { show: false, msg: '', color: '' },
-  loading: { show: false, msg: '' },
-  connectedAccount: '',
+  alert: { show: false, msg: "", color: "" },
+  loading: { show: false, msg: "" },
+  ownerAccount: "",
+  connectedAccount: "",
+  maxSupply: 0,
+  cost: 0,
   contract: null,
   nfts: [],
-})
+});
 
 const truncate = (text, startChars, endChars, maxLength) => {
   if (text.length > maxLength) {
-    let start = text.substring(0, startChars)
-    let end = text.substring(text.length - endChars, text.length)
+    let start = text.substring(0, startChars);
+    let end = text.substring(text.length - endChars, text.length);
     while (start.length + end.length < maxLength) {
-      start = start + '.'
+      start = start + ".";
     }
-    return start + end
+    return start + end;
   }
-  return text
-}
+  return text;
+};
 
-const setAlert = (msg, color = 'green') => {
-  setGlobalState('alert', { show: true, msg, color })
+const setAlert = (msg, color = "green") => {
+  setGlobalState("alert", { show: true, msg, color });
   setTimeout(() => {
-    setGlobalState('alert', { show: false, msg: '', color })
-    setGlobalState('loading', false)
-  }, 8000)
-}
+    setGlobalState("alert", { show: false, msg: "", color });
+    setGlobalState("loading", false);
+  }, 8000);
+};
 
 const setLoadingMsg = (msg) => {
-  const loading = getGlobalState('loading')
-  setGlobalState('loading', { ...loading, msg })
-}
+  const loading = getGlobalState("loading");
+  setGlobalState("loading", { ...loading, msg });
+};
 
 export {
   useGlobalState,
@@ -40,4 +43,4 @@ export {
   truncate,
   setAlert,
   setLoadingMsg,
-}
+};
