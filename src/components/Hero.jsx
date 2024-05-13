22d6f8ae14321b5e8b273e3c00b35786e41818b5
lucas-po <lucaschang1127@gmail.com>
2024-05-14 05:26:36 +0900

    reafactor: update hero component for admin ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,17 @@
+import React, { useState, useEffect } from "react";
+
 import { setAlert, setGlobalState, useGlobalState } from "../store";
-import { payToMint } from "../Minting";
+import { payToMint, setMaxSupply, setCost } from "../Minting";
 
 const Hero = () => {
   const [nfts] = useGlobalState("nfts");
 
+  const [ownerAccount] = useGlobalState("ownerAccount");
+  const [connectedAccount] = useGlobalState("connectedAccount");
+  const [maxSupply] = useGlobalState("maxSupply");
+  const [cost] = useGlobalState("cost");
+
+  console.log(maxSupply, cost);
   const onMintNFT = async () => {
     setGlobalState("loading", {
       show: true,
@@ -15,18 +23,94 @@ const Hero = () => {
       .catch(() => setGlobalState("loading", { show: false, msg: "" }));
   };
 
-  console.log(nfts.length);
+  const handleSubmitMaxSupply = async (event) => {
+    event.preventDefault();
+    const new_maxSupply = document.getElementById("maxSupply").value;
+
+    if (new_maxSupply <= 0) return;
+    setGlobalState("loading", {
+      show: true,
+      msg: "Setting the maxSupply",
+    });
+    await setMaxSupply(new_maxSupply)
+      .then(() => setAlert("Setting maxSupply Successful...", "green"))
+      .catch(() => setGlobalState("loading", { show: false, msg: "" }));
+  };
+
+  const handleSubmitCost = async (event) => {
+    event.preventDefault();
+    const new_maxSupply = document.getElementById("maxSupply").value;
+    const new_cost = document.getElementById("cost").value;
+
+    if (new_cost <= 0) return;
+    setGlobalState("loading", {
+      show: true,
+      msg: "Setting the cost",
+    });
+    await setCost(new_cost)
+      .then(() => setAlert("Setting cost Successful...", "green"))
+      .catch(() => setGlobalState("loading", { show: false, msg: "" }));
+    // Perform any desired actions with the form data
+  };
+
   return (
     <div className="flex h-full">
       <div className="flex flex-col justify-center items-center mx-auto py-24">
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-white text-5xl font-bold text-center">
-            <span className="text-gradient">NFTs</span> Minting
+            {ownerAccount === connectedAccount ? (
+              <span>Admin</span>
+            ) : (
+              <>
+                <span className="text-gradient">NFTs</span> Minting
+              </>
+            )}
           </h1>
 
           <p className="text-white font-semibold text-sm mt-3">
             Mint the hottest NFTs around.
           </p>
+          {ownerAccount === connectedAccount && (
+            <div className="py-2">
+              <form onSubmit={handleSubmitMaxSupply} className="flex flex-col">
+                <label className="text-md text-white font-semibold py-1">
+                  MaxSupply
+                </label>
+                <input
+                  type="number"
+                  id="maxSupply"
+                  placeholder={maxSupply}
+                  min={1}
+                  // defaultValue={maxSupply}
+                />
+
+                <button
+                  type="submit"
+                  className="text-white bg-blue-400 py-2 rounded-full mt-2"
+                >
+                  Submit
+                </button>
+              </form>
+              <form onSubmit={handleSubmitCost} className="flex flex-col">
+                <label className="text-md text-white font-semibold py-1">
+                  Cost
+                </label>
+                <input
+                  type="number"
+                  id="cost"
+                  placeholder={cost}
+                  // defaultValue={cost}
+                  step={0.001}
+                />
+                <button
+                  type="submit"
+                  className="text-white bg-blue-400 py-2 rounded-full mt-2"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          )}
 
           <button
             className="shadow-xl shadow-black text-white
@@ -44,7 +128,9 @@ const Hero = () => {
             hover:bg-[#bd255f] hover:text-white transition-all
             duration-75 delay-100"
           >
-            <span className="text-xs font-bold">{nfts.length}/99</span>
+            <span className="text-xs font-bold">
+              {nfts.length}/{maxSupply}
+            </span>
           </div>
         </div>
       </div>
